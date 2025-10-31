7fc9762c37bd294ee7ba1dfc60c238df9b079f3c
vishalghorpade2108 <133584503+vishalghorpade2108@users.noreply.github.com>
2025-10-31 12:30:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -36,4 +36,4 @@ app.get("/", (req, res) => {
   res.send("Hospital Management Backend Running...");
 });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT} and `));
